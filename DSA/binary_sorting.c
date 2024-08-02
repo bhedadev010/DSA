7774bb959f978a4601f5e8e31c0416b1953a21b5
Dev Bheda <bhedadev010@gmail.com>
2024-08-02 17:19:45 +0530

    2-8-24

---
+++ DSA/binary_sorting.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int binary_search(int arr[],int size,int element)
+{
+    int low=0,mid,high=size-1;
+    
+    while(low<=high)
+    {
+        mid = (high+low)/2;
+        if(arr[mid]==element)
+        {
+            printf("Element %d found at index %d",arr[mid],mid+1);
+            return 1;
+        }
+        else if(element<arr[mid])
+        {
+            high = mid-1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    printf("Element not found!!!");
+}
+
+int main(){
+int element= 20;
+int arr[5]={10,20,30,40,50};
+int size = sizeof(arr)/sizeof(int);
+binary_search(arr,size,element);
+return 0;
+
+}
